9b48293870dc3bb21b48593a25278da50d987b5c
AymanNasser <aymannasr43@gmail.com>
2020-04-24 22:38:58 +0200

    labelBar v3

--- LabelBar.py
+++ LabelBar.py
@@ -1,32 +1,29 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-import  random
-import imutils
-
 
 # Tuning Parameters
 sigma = 0.33 # Canny's formula
 houghLineLength = 100
-houghLineGap = 20
-dilationIterations = 5
+houghLineGap = 80
+dilationIterations = 3
 erosionIterations = 1
-epsilonFactor = 0.01
+epsilonFactor = 0.03
 minAspectRatio = 0.5
 maxAspectRatio = 4
 rectMinApprox = 4
 rectMaxApprox = 10
 minAreaFilter = 5000
 
+img = cv.imread('data/projectTes.jpeg', cv.IMREAD_COLOR)
 
-
-def labelBarDetection(srcImagePath):
+def labelBarDetection(img):
 
     # Image Preparation & Kernel defintion of 5*5 Matrix
-    img = cv.imread(srcImagePath, cv.IMREAD_COLOR)
+
     grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     # Automatic canny edge detection using sigma value = 0.33
     v = np.median(grayImg)
     lower = int(max(0, (1.0 - sigma) * v))
@@ -48,7 +45,7 @@ def labelBarDetection(srcImagePath):
     erode = cv.erode(processingImg, kernel, iterations=erosionIterations)
 
     # Finding contours
-    contours, hierarchy = cv.findContours(erode, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(processingImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # Filtering founded contours
     filtered = []
@@ -57,28 +54,26 @@ def labelBarDetection(srcImagePath):
         filtered.append(c)
 
     # print(len(filtered))
-    detectedLabelBars = [[0 for x in range(len(filtered))] for y in range(4)]
-
+    #detectedLabelBars = [[0 for x in range(len(filtered))] for y in range(4)]
+    counter = 0
     # Drawing contours
-    for cont in filtered:
+    for cont in contours:
         epsilon = epsilonFactor * cv.arcLength(cont, True)
         approx = cv.approxPolyDP(cont, epsilon, True)
-        counter = 0
-        x, y, w, h = cv.boundingRect(cont)
-        aspectRatio = float(w) / h
-        # Checking if it is a rectangle
-        if (aspectRatio >= minAspectRatio and aspectRatio <= maxAspectRatio) and \
-                (len(approx) >= rectMinApprox and len(approx) <= rectMaxApprox):
 
-            cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            detectedLabelBars[counter].append(x,y,w,h)
-            counter += 1
+        x, y, w, h = cv.boundingRect(cont)
+
+        print(len(approx))
+        # Checking if it is a rectangle
+        if len(approx) == 4:
+            if(hierarchy.ravel()[counter*4 + 2] != -1):
+                cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                #detectedLabelBars[counter].append(x,y,w,h)
+
+        counter += 1
 
         # col = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         # cv.drawContours(img, [approx], -1, col, 5)
-
-
-    return detectedLabelBars
 
     titles = ['Orignal','Gray','Canny','Processing','erode','dil']
     images = [img,grayImg,edges,processingImg,erode,dil]
@@ -91,7 +86,10 @@ def labelBarDetection(srcImagePath):
         plt.title(titles[ite])
     plt.show()
 
+    #return detectedLabelBars
 
+
+labelBarDetection(img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
