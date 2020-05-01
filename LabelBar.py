f73986f64e0f729a0bc74b4ba7987c5462db93d6
AymanNasser <aymannasr43@gmail.com>
2020-05-01 17:57:12 +0200

    working version of labelBar

--- LabelBar.py
+++ LabelBar.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 
 # Tuning Parameters
 sigma = 0.33 # Canny's formula
+approxComparisonValue = 4
 houghLineLength = 100
 houghLineGap = 10
 dilationIterations = 3
@@ -11,9 +12,33 @@ erosionIterations = 2
 epsilonFactor = 0.03
 houghLineThickness = 2
 minContourArea = 2000
+minDiff_XPoints = 20
 
+img = cv.imread('data/test.png', cv.IMREAD_COLOR)
 
-img = cv.imread('data/pic3.png', cv.IMREAD_COLOR)
+def sortContours(contours):
+
+    proccessingContours = [[0 for i in range(2)] for j in range(4)]
+
+    for ite in range(len(contours)):
+        proccessingContours[ite][0]= (contours[ite][0][0])
+        proccessingContours[ite][1]= (contours[ite][0][1])
+    # Sorting founded contours by x-axis points
+    proccessingContours.sort(key=lambda x:x[0])
+    return proccessingContours
+
+def  filterContours(contours):
+
+    proccessingContours = [[0 for i in range(2)] for j in range(4)]
+    proccessingContours = sortContours(contours)
+
+    verticalLineDiff1 = proccessingContours[1][0] - proccessingContours[0][0]
+    verticalLineDiff2 = proccessingContours[3][0] - proccessingContours[2][0]
+    # Checking if the rectangle edges aren't quite vertical or not
+    if(verticalLineDiff1 > minDiff_XPoints or verticalLineDiff2 > minDiff_XPoints):
+        return False
+    else:
+        return True
 
 def labelBarDetection(img):
 
@@ -44,7 +69,7 @@ def labelBarDetection(img):
 
     # Finding contours
     contours, hierarchy = cv.findContours(processingImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    #print(len(contours))
+
     detectedLabelBars = {}
     hierarchyCounter = 0
     dictionaryCounter = 0
@@ -54,37 +79,16 @@ def labelBarDetection(img):
         approx = cv.approxPolyDP(cont, epsilon, True)
 
         x, y, w, h = cv.boundingRect(cont)
-
-        #print(len(approx))
         # Checking if it is a rectangle
-        if len(approx) == 4 and cv.contourArea(cont) > minContourArea:
-            if(hierarchy.ravel()[hierarchyCounter*4 + 2] == -1 ):#and hierarchy.ravel()[hierarchyCounter*4 + 3] == -1):
+        if len(approx) == approxComparisonValue  and cv.contourArea(cont) > minContourArea and filterContours(approx) == True:
+            if(hierarchy.ravel()[hierarchyCounter*4 + 2] == -1 ):
                 #print(hierarchy.ravel())
                 cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 detectedLabelBars[dictionaryCounter] = [x+w/2,y-h/2,w,h]
                 dictionaryCounter += 1
         hierarchyCounter += 1
 
-        # col = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        # cv.drawContours(img, [approx], -1, col, 5)
-
-    titles = ['Orignal','Gray','Canny','Processing','dil']
-    images = [img,grayImg,edges,processingImg,dil]
-
-    rows = 2
-    columns = 4
-    iterations = len(images)
-    for ite in range(iterations):
-        plt.subplot(rows,columns, ite+1 ), plt.imshow(images[ite])
-        plt.title(titles[ite])
-    plt.show()
-
     return detectedLabelBars
-
-x = {}
-
-x = labelBarDetection(img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
-
