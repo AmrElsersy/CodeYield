85c15056b0f579e07f495210c19e27057583670a
AhmedMoamen62 <ahmedmoamen138@gmail.com>
2020-05-06 20:42:41 +0200

    working with independent ip address

--- codeyield/server.py
+++ codeyield/server.py
@@ -4,6 +4,7 @@ from Controlfunction import imageprocessing
 import base64
 import time
 import os
+import socket
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def upload():
     
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.5',port=5000)
+    ip_address = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    app.run(debug=True, host=ip_address,port=5000)
